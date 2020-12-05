97884f15eb58af9d0cfb9cfaea42046ff07ce008
Leonnardd <71668775+Leonnardd@users.noreply.github.com>
2020-12-05 13:56:02 -0300

    Sistema de Visualização de imagens concluído em sua base

--- 666RouteProjectURP_/Assets/Scripts/SelectScript.cs
+++ 666RouteProjectURP_/Assets/Scripts/SelectScript.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SelectScript : MonoBehaviour
 {
     [SerializeField]private Material mat;
     private Transform _selection;
     private Material defMat;
+    public GameObject ImgSee;
+    public Image img;
+    private bool paused=false;
     void Update()
     {
         if(_selection != null){
@@ -20,10 +24,24 @@ public class SelectScript : MonoBehaviour
             var selection = hit.transform;
         
             if(selection.CompareTag("Selectable")){
+                if(Input.GetKeyDown(KeyCode.E)){
+                    paused = !paused;
+                    if(paused){
+                        ImgSee.SetActive(true);
+                        img.sprite = selection.GetComponent<Selectable>().img;
+                        Time.timeScale = 0f;
+                    }
+                    else{
+                        ImgSee.SetActive(false);
+                        Time.timeScale = 1f;
+                    }
+                }
                 var selectionRenderer = selection.GetComponent<Renderer>();
                 if(selectionRenderer != null){
-                    defMat = selectionRenderer.material;
+                    if(selectionRenderer.material != mat){
+                        defMat = selectionRenderer.material;
                     selectionRenderer.material = mat;
+                    }   
                 }
                 _selection = selection;
             }
